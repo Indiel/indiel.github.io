5a471e67bca0e2040fbf5c9e4415e8a90f206d92
Indiel <kori.yanagi@gmail.com>
2018-09-23 00:16:03 +0300

    Исправлена страница каталога

--- Gllacy/js/media.js
+++ Gllacy/js/media.js
@@ -8,9 +8,6 @@
     var smallUserNav = document.querySelector('.small-user-nav');
     var userNav = document.querySelector('.user-nav');
 
-    // var smallCatalogNav = document.querySelector('.site-nav a[href="catalog.html"]');
-    // var catalogNav = document.querySelector('.site-nav-catalog');
-
     smallSiteNav.addEventListener('click', () => {
         userNav.classList.remove('show-small-nav');
         sitaNav.classList.toggle('show-small-nav');
@@ -20,17 +17,5 @@
         sitaNav.classList.remove('show-small-nav');
         userNav.classList.toggle('show-small-nav');
     });
-
-    // smallCatalogNav.addEventListener('click', evt => {
-    //     if (document.documentElement.clientWidth <= 900) {
-    //         evt.preventDefault();
-    //         catalogNav.classList.toggle('show-small-nav');
-    //     }
-    // });
-
-    // if (document.documentElement.clientWidth > 900) {
-    //     catalogNav.classList.remove('show-small-nav');
-    //     sitaNav.classList.remove('show-small-nav');
-    // }
 
 })();

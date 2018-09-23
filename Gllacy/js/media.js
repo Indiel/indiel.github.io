2183c730c71ad45daa32d849a84ad9a4aa207282
Indiel <kori.yanagi@gmail.com>
2018-09-23 12:49:00 +0300

    Фильтры товаров скрыты, добавлена кнопка

--- Gllacy/js/media.js
+++ Gllacy/js/media.js
@@ -8,6 +8,9 @@
     var smallUserNav = document.querySelector('.small-user-nav');
     var userNav = document.querySelector('.user-nav');
 
+    var spanAfter = document.querySelector('.after');
+    var spanBefore = document.querySelector('.before');
+
     smallSiteNav.addEventListener('click', () => {
         userNav.classList.remove('show-small-nav');
         sitaNav.classList.toggle('show-small-nav');
@@ -16,6 +19,24 @@
     smallUserNav.addEventListener('click', () => {
         sitaNav.classList.remove('show-small-nav');
         userNav.classList.toggle('show-small-nav');
+        spanAfter.classList.toggle('rotate-after');
+        spanBefore.classList.toggle('rotate-before');
+    });
+
+    window.addEventListener('resize', () => {
+        if (document.documentElement.clientWidth > 900) {
+            sitaNav.classList.remove('show-small-nav');
+        }
+        if (document.documentElement.clientWidth > 550) {
+            userNav.classList.remove('show-small-nav');
+        }
+    });
+
+    var filtersForm = document.querySelector('.filters-form');
+    var buttonFilters = document.querySelector('.button-filters');
+    
+    buttonFilters.addEventListener('click', () => {
+        filtersForm.classList.toggle('show-button');
     });
 
 })();

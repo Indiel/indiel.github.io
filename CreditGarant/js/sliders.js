62dc913294cbe5fc5b453a8744b02ba269ff9f98
Indiel <kori.yanagi@gmail.com>
2018-10-24 18:03:47 +0300

    credit-rating

--- CreditGarant/js/sliders.js
+++ CreditGarant/js/sliders.js
@@ -32,7 +32,84 @@
             this.list.style.left = this.shift + 'px';
         });
     };
+
+
+
+    // var mainPin = document.querySelector('.map__pin--main');
+    // var adForm = document.querySelector('.ad-form');
+  
+    // var addressInput = document.querySelector('#address');
+    // addressInput.value = (mainPin.offsetLeft + 32) + ', ' + (mainPin.offsetTop + 32);
+  
+    // mainPin.addEventListener('mousedown', function (evt) {
+    //     evt.preventDefault();
+    
+    //     var startCoords = {
+    //         x: evt.clientX,
+    //         y: evt.clientY
+    //     };
+        
+    //     // активация сайта
+    //     var onMainPinActiveteSite = function () {
+    //         document.querySelector('.map').classList.remove('map--faded');
+    
+    //         window.backend.download(onLoad, window.backend.onError);
+    
+    //         for (var j = 0; j < fieldsets.length; j++) {
+    //             fieldsets[j].disabled = false;
+    //         }
+    //         for (var l = 0; l < mapFilters.length; l++) {
+    //             mapFilters[l].disabled = false;
+    //         }
+    //         addressInput.value = (mainPin.offsetLeft + 32) + ', ' + (mainPin.offsetTop + 85);
+    //         adForm.classList.remove('ad-form--disabled');
+    //         mainPin.removeEventListener('mouseup', onMainPinActiveteSite);
+    
+    //         window.onformRoomChange();
+    //     };
+    
+    //     var onMouseMove = function (moveEvt) {
+    //         moveEvt.preventDefault();
+    
+    //         var shift = {
+    //             x: startCoords.x - moveEvt.clientX,
+    //             y: startCoords.y - moveEvt.clientY
+    //         };
+    
+    //         startCoords = {
+    //             x: moveEvt.clientX,
+    //             y: moveEvt.clientY
+    //         };
+    
+    //         var pinCoords = {
+    //         x: mainPin.offsetLeft - shift.x,
+    //         y: mainPin.offsetTop - shift.y
+    //         };
+    
+    //         if (pinCoords.y >= (130 - 65) && pinCoords.y <= 630) {
+    //         mainPin.style.left = pinCoords.x + 'px';
+    //         mainPin.style.top = pinCoords.y + 'px';
+    //         addressInput.value = (pinCoords.x + 32) + ', ' + (pinCoords.y + 85);
+    //         }
+    //     };
+    
+    //     var onMouseUp = function (upEvt) {
+    //         upEvt.preventDefault();
+    
+    //         mapPins.removeEventListener('mousemove', onMouseMove);
+    //         mapPins.removeEventListener('mouseup', onMouseUp);
+    //     };
+    
+    //     if (document.querySelector('.map').classList.contains('map--faded')) {
+    //         onMainPinActiveteSite();
+    //     }
+    
+    //     mapPins.addEventListener('mousemove', onMouseMove);
+    //     mapPins.addEventListener('mouseup', onMouseUp);
+    // });
       
+
+
     var offer = new Slider (
         document.querySelector('.offer__button-prev'),
         document.querySelector('.offer__button-next'),

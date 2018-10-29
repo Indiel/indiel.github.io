8f74b79c54ca002783d49fab1f3b1fd998b30612
Indiel <kori.yanagi@gmail.com>
2018-10-29 13:33:00 +0200

    menu

--- CreditGarant/js/menu.js
+++ CreditGarant/js/menu.js
@@ -6,24 +6,10 @@
     var mainNav = document.querySelector(".main-nav");
 
     if (document.documentElement.clientWidth <= 576) {
-        mainNav.classList.add('visually-hidden');
 
         hamburger.addEventListener("click", function() {
             hamburger.classList.toggle("is-active");
-            mainNav.classList.toggle("visually-hidden");
-    
-            // mainNavWrapper.classList.toggle('main-nav__wrapper--active');
-    
-            // document.querySelector("body").classList.toggle("none-scroll");
-    
-            // Array.from(navListLink).forEach((elem) => {
-            //     elem.addEventListener("click", function () {
-            //         hamburger.classList.remove("is-active");
-            //         mainNavWrapper.classList.remove('main-nav__wrapper--active');
-            //         // langList.classList.add('visually-hidden');
-            //         document.querySelector("body").classList.remove("none-scroll");
-            //     });
-            // });
+            mainNav.classList.toggle("main-nav--active");
         });
     
         window.addEventListener('resize', () => {

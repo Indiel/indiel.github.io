729f18c5e00f20de96dbdc88b465fcc0a974f0cb
Indiel <kori.yanagi@gmail.com>
2018-10-30 13:04:52 +0200

    tablet

--- CreditGarant/js/menu.js
+++ CreditGarant/js/menu.js
@@ -4,6 +4,15 @@
 
     var hamburger = document.querySelector(".hamburger");
     var mainNav = document.querySelector(".main-nav");
+    var mainNavList = document.querySelector(".main-nav__list");
+
+    if (document.documentElement.clientWidth > 576 && document.documentElement.clientWidth <= 1200) {
+
+        hamburger.addEventListener("click", function() {
+            hamburger.classList.toggle("is-active");
+            mainNavList.classList.toggle("main-nav__list--active");
+        });
+    }
 
     if (document.documentElement.clientWidth <= 576) {
 

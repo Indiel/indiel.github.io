c475568f44310b44611e7e7524db7fb8d80900e3
Indiel <kori.yanagi@gmail.com>
2018-11-23 12:50:02 +0200

    scroll

---
+++ Viral2.1/js/scroll.js
@@ -0,0 +1,101 @@
+'use strict';
+
+(function () {
+
+    var sections = document.querySelectorAll('section');
+    // var html = document.querySelectorAll('body');
+
+    // var heights = [];
+    var offsets = [];
+    // var count = 0;
+    var isAnimating  = false;
+
+    Array.from(sections).forEach((elem, i) => {
+        // heights[i] = elem;
+        offsets[i] = elem.offsetTop;
+    });
+
+    window.addEventListener('wheel', (evt) => {
+        // evt.preventDefault();
+        // evt.stopPropagation();
+
+        if (isAnimating) {
+            return false;
+        }
+
+        for (var i = 0; i < offsets.length; i++) {
+            if (window.pageYOffset + 100 >= offsets[offsets.length - 1]) {
+                return;
+            } else if (window.pageYOffset + 100 === offsets[i]) {
+                evt.preventDefault();
+                evt.stopPropagation();
+
+                if (evt.wheelDelta < 0) {
+                    isAnimating  = true;
+                    $('html, body').animate({
+                        scrollTop: offsets[i + 1] - 100
+                    }, 500, 'swing', function(){
+                        isAnimating  = false;
+                    });
+                    return;
+                } else {
+                    isAnimating  = true;
+                    $('html, body').animate({
+                        scrollTop: offsets[i - 1] - 100
+                    }, 500, 'swing', function(){
+                        isAnimating  = false;
+                    });
+                    return;
+                }
+            } else if (window.pageYOffset + 100 > offsets[i] && window.pageYOffset + 100 < offsets[i + 1]) {
+                evt.preventDefault();
+                evt.stopPropagation();
+
+                if (evt.wheelDelta < 0) {
+                    isAnimating  = true;
+                    $('html, body').animate({
+                        scrollTop: offsets[i + 1] - 100
+                    }, 500, 'swing', function(){
+                        isAnimating  = false;
+                    });
+                    return;
+                } else {
+                    isAnimating  = true;
+                    $('html, body').animate({
+                        scrollTop: offsets[i] - 100
+                    }, 500, 'swing', function(){
+                        isAnimating  = false;
+                    });
+                    return;
+                }
+            }
+        }
+
+        // offsets.forEach((elem, i) => {
+        //     if (window.pageYOffset >= elem && window.pageYOffset < elem)
+        // });
+        
+        // if (evt.wheelDelta < 0) {
+        //     if (count < (heights.length - 1)) {
+        //         count++;
+        //         isAnimating  = true;
+        //         $('html, body').animate({
+        //             scrollTop: $(heights[count]).offset().top - 100
+        //         }, 500, 'swing', function(){
+        //             isAnimating  = false;
+        //         });
+        //     }
+        // } else {
+        //     if (count > 0) {
+        //         count--;
+        //         isAnimating  = true;
+        //         $('html, body').animate({
+        //             scrollTop: $(heights[count]).offset().top - 100
+        //         }, 500, 'swing', function(){
+        //             isAnimating  = false;
+        //         });
+        //     }
+        // }
+    });
+
+})();

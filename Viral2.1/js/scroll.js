a1ea9e600ddb0c7b696c3e54820c33ed734a3768
Indiel <kori.yanagi@gmail.com>
2018-11-28 11:56:34 +0200

    progress bar

--- Viral2.1/js/scroll.js
+++ Viral2.1/js/scroll.js
@@ -10,14 +10,32 @@
     // var count = 0;
     var isAnimating  = false;
 
+    var count = 0;
+    var oldCount = count;
+
+    var sectionNumber = function() {
+        for (let i = 0; i < offsets.length; i++) {
+            if (window.pageYOffset + 100 >= offsets[offsets.length - 1]) {
+                count = offsets.length - 1;
+                return count;
+            } else if (window.pageYOffset + 100 >= offsets[i] && window.pageYOffset + 100 < offsets[i + 1]) {
+            // } else if ((window.pageYOffset + 100 >= offsets[i] || window.pageYOffset + document.documentElement.clientHeight >= offsets[i]) && (window.pageYOffset + 100 < offsets[i + 1] || window.pageYOffset + document.documentElement.clientHeight < offsets[i + 1])) {
+                count = i;
+                return count;
+            }
+        }
+    };
+
     setTimeout(function() {
         Array.from(sections).forEach((elem, i) => {
             heights[i] = elem;
             offsets[i] = elem.offsetTop;
         });
-        console.log(heights);
-        console.log(offsets);
-    }, 3000);
+        
+        sectionNumber();
+        buttons[count].classList.add('line-bottom');
+    
+    }, 2000);
 
 
     var buttonsNode = document.querySelectorAll('.progress-bar__item');
@@ -25,18 +43,69 @@
     Array.from(buttonsNode).forEach((elem, i) => {
         buttons[i] = elem;
     });
+    console.log(buttons);
+  
+    var removeClasses = function(elem) {
+        // for (let j = 1; j < buttons[elem].classList.length; j++) {
+        //     buttons[elem].classList.remove(buttons[elem].classList[j]);
+        //     // buttons[elem - 1].classList.remove(buttons[elem - 1].classList[j]);
+        //     buttons[elem + 1].classList.remove(buttons[elem + 1].classList[j]);
+        // }
+        buttons.forEach((elem) => {
+            // buttons[i] = elem;
+            for (let j = 1; j < elem.classList.length; j++) {
+                elem.classList.remove(elem.classList[j]);
+            }
+        });
+    };
 
-    var pageYOffset;
-    window.addEventListener('scroll', (evt) => {
-        // console.log(evt);
-        pageYOffset = window.pageYOffset;
-        console.log(pageYOffset);
-    });
-    
+
+    // var oldPageYOffset = window.pageYOffset;
+
+    window.addEventListener('scroll', () => {
+        sectionNumber();
+        console.log(count);
+
+        if (window.pageYOffset + 100 >= offsets[offsets.length]) {
+            for (let j = (buttons[oldCount].classList.length - 1); j >= 1; j--) {
+                buttons[oldCount].classList.remove(buttons[oldCount].classList[j]);
+            }
+            buttons[count].classList.add('line-bottom');
+        } else
+        // if (window.pageYOffset > oldPageYOffset) {
+        if (count > oldCount) {
+            // removeClasses();
+            buttons[oldCount].classList.add('none-active-line-bottom');
+            for (let j = (buttons[count].classList.length - 1); j >= 1; j--) {
+                buttons[count].classList.remove(buttons[count].classList[j]);
+            }
+
+            buttons[count].classList.add('line-bottom');
+            // buttons[oldCount].classList.add('none-active-line-bottom');
+        // } else if (window.pageYOffset < oldPageYOffset) {
+        } else if (count < oldCount) {
+            // removeClasses();
+            for (let j = (buttons[count].classList.length - 1); j >= 1; j--) {
+                buttons[count].classList.remove(buttons[count].classList[j]);
+            }
+            for (let j = (buttons[oldCount].classList.length - 1); j >= 1; j--) {
+                buttons[oldCount].classList.remove(buttons[oldCount].classList[j]);
+            }              
+            buttons[count].classList.add('line-top');         
+        }
+        
+        // oldPageYOffset = window.pageYOffset;
+        oldCount = count;
+    })
 
     window.addEventListener('wheel', (evt) => {
         // evt.preventDefault();
         // evt.stopPropagation();
+        // buttons.forEach((elem) => {
+        //     for (let j = 1; j < elem.classList.length; j++) {
+        //         elem.classList.remove(elem.classList[j]);
+        //     }
+        // });
 
         if (isAnimating) {
             return false;
@@ -50,8 +119,16 @@
                 evt.stopPropagation();
 
                 if (evt.wheelDelta < 0) {
-                    buttons[i + 1].classList.add('line-bottom');
-                    buttons[i + 1].classList.add('none-active-line-bottom');
+
+                    // removeClasses(i);
+                    // // for (let j = 1; j < buttons[i].classList.length; j++) {
+                    // //     buttons[i].classList.remove(buttons[i].classList[j]);
+                    // // }
+                    // // buttons[i].classList.remove('line-top');
+                    // // buttons[i].classList.remove(buttons[i].classList[1]);
+                    // buttons[i + 1].classList.add('line-bottom');
+                    // // console.log(buttons[i + 1].classList[1]);
+                    // buttons[i].classList.add('none-active-line-bottom');
 
                     isAnimating  = true;
                     $('html, body').animate({
@@ -61,7 +138,13 @@
                     });
                     return;
                 } else {
-                    buttons[i].classList.add('line-top');
+                    // removeClasses(i);
+                    // // for (let j = 1; j < buttons[i].classList.length; j++) {
+                    // //     buttons[i + 1].classList.remove(buttons[i + 1].classList[j]);
+                    // // }
+                    // // buttons[i + 1].classList.remove(buttons[i + 1].classList[1]);
+                    // // buttons[i + 1].classList.remove('line-bottom');                    
+                    // buttons[i].classList.add('line-top');
 
                     isAnimating  = true;
                     $('html, body').animate({
@@ -76,8 +159,16 @@
                 evt.stopPropagation();
 
                 if (evt.wheelDelta < 0) {
-                    buttons[i + 1].classList.add('line-bottom');
-                    buttons[i + 1].classList.add('none-active-line-bottom');
+                    // removeClasses(i);
+                    // // for (let j = 1; j < buttons[i].classList.length; j++) {
+                    // //     buttons[i].classList.remove(buttons[i].classList[j]);
+                    // // }
+                    // // buttons[i].classList.remove('line-top');
+                    // // buttons[i + 1].classList.remove('line-top');
+                    // // buttons[i].classList.remove(buttons[i].classList[1]);
+
+                    // buttons[i + 1].classList.add('line-bottom');
+                    // buttons[i].classList.add('none-active-line-bottom');
 
                     isAnimating  = true;
                     $('html, body').animate({
@@ -87,7 +178,14 @@
                     });
                     return;
                 } else {
-                    buttons[i].classList.add('line-top');                    
+                    // removeClasses(i);
+                    // // for (let j = 1; j < buttons[i].classList.length; j++) {
+                    // //     buttons[i + 1].classList.remove(buttons[i + 1].classList[j]);
+                    // // }
+                    // // buttons[i + 1].classList.remove(buttons[i + 1].classList[1]);
+
+                    // // buttons[i + 1].classList.remove('line-bottom');                    
+                    // buttons[i].classList.add('line-top');                    
 
                     isAnimating  = true;
                     $('html, body').animate({
@@ -99,34 +197,6 @@
                 }
             }
         }
-
-        // console.log(window.offsetTop);
-
-        // offsets.forEach((elem, i) => {
-        //     if (window.pageYOffset >= elem && window.pageYOffset < elem)
-        // });
-        
-        // if (evt.wheelDelta < 0) {
-        //     if (count < (heights.length - 1)) {
-        //         count++;
-        //         isAnimating  = true;
-        //         $('html, body').animate({
-        //             scrollTop: $(heights[count]).offset().top - 100
-        //         }, 500, 'swing', function(){
-        //             isAnimating  = false;
-        //         });
-        //     }
-        // } else {
-        //     if (count > 0) {
-        //         count--;
-        //         isAnimating  = true;
-        //         $('html, body').animate({
-        //             scrollTop: $(heights[count]).offset().top - 100
-        //         }, 500, 'swing', function(){
-        //             isAnimating  = false;
-        //         });
-        //     }
-        // }
     });
 
 })();
